574cf6786b2d48afadc9135914bccb9b97ca51e5
Jon Carlson <jon@jomca.co>
2014-07-08 17:52:51 -0600

    Updates

---
+++ data.js
@@ -0,0 +1,39 @@
+// Define variable to store data
+var quoteData = [];
+
+quoteData.push({
+	text: 'Live as if you were to die tomorrow. Learn as if you were to live forever.',
+	author: 'Mahatma Gandhi',
+	views: 0,
+	rating: 0
+	});
+quoteData.push({
+	text: 'If one does not know to which port one is sailing, no wind is favorable.',
+	author: 'Lucius Annaeus Seneca',
+	views: 0,
+	rating: 0
+	});
+quoteData.push({
+	text: 'Your time is limited, so don’t waste it living someone else’s life.',
+	author: 'Steve Jobs',
+	views: 0,
+	rating: 0
+	});
+quoteData.push({
+	text: 'The two most important days in your life are the day you are born and the day you find out why.',
+	author: 'Mark Twain',
+	views: 0,
+	rating: 0
+	});
+quoteData.push({
+	text: 'My optimism wears heavy boots and is loud.',
+	author: 'Henry Rollins',
+	views: 0,
+	rating: 0
+	});
+quoteData.push({
+	text: 'Whenever you find yourself on the side of the majority, it is time to pause and reflect.',
+	author: 'Mark Twain',
+	views: 0,
+	rating: 0
+	});
---
+++ main.js
@@ -0,0 +1,51 @@
+// Render the quote data on the page
+// layoutStyle is 1(list) or 2(panels)
+var renderData = function(layoutStyle) {
+	// console.log("Rendering the data with layout style: " + layoutStyle);
+	$('.data').empty();
+	console.log(quoteData);
+	for (var i = 0; i <= quoteData.length - 1; i++) {
+
+		var newDataItem = $('<div class="quote-data">');
+		newDataItem.append(
+			'<div>' + quoteData[i].text + '</div>'
+		);
+		newDataItem.append(
+			'<div>-' + quoteData[i].author + '</div>'
+		);
+		newDataItem.append(
+			'<div>Rating: ' + quoteData[i].rating + '</div>'
+		);
+		// Create new DOM element
+		newDataItem.appendTo('.data');
+
+	};
+}
+
+var hideInputs = function() {
+	$('.entry-form').hide();
+	$('.filter-form').hide();
+}
+
+
+
+
+$(document).on('ready', function() {
+
+	hideInputs();
+	renderData(1);
+
+	$('#home').on('click', function() {
+		hideInputs();
+		renderData(1);
+	})
+	$('#add').on('click', function() {
+		hideInputs();
+		$('.entry-form').slideDown(100);
+	})
+	$('#filter').on('click', function() {
+		hideInputs();
+		$('.filter-form').slideDown(100);
+	})
+  
+});
