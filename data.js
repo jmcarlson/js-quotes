6ad278a40fe0c8ab53ba675cacd7888e4f2833d2
Jon Carlson <jon@jomca.co>
2014-07-10 18:16:20 -0600

    Updates

--- data.js
+++ data.js
@@ -1,39 +1,53 @@
 // Define variable to store data
 var quoteData = [];
+var searchData = [];
 
 quoteData.push({
-	text: 'Live as if you were to die tomorrow. Learn as if you were to live forever.',
-	author: 'Mahatma Gandhi',
+	key: 1,
+	text: 'No matter where you go, there you are.',
+	author: 'Buckaroo Bonzai',
 	views: 0,
-	rating: 0
+	rating: 5
 	});
 quoteData.push({
+	key: 2,
 	text: 'If one does not know to which port one is sailing, no wind is favorable.',
 	author: 'Lucius Annaeus Seneca',
 	views: 0,
-	rating: 0
+	rating: 4
 	});
 quoteData.push({
+	key: 3,
 	text: 'Your time is limited, so don’t waste it living someone else’s life.',
 	author: 'Steve Jobs',
 	views: 0,
-	rating: 0
+	rating: 3
 	});
 quoteData.push({
+	key: 4,	
 	text: 'The two most important days in your life are the day you are born and the day you find out why.',
 	author: 'Mark Twain',
 	views: 0,
-	rating: 0
+	rating: 4
 	});
 quoteData.push({
+	key: 5,
 	text: 'My optimism wears heavy boots and is loud.',
 	author: 'Henry Rollins',
 	views: 0,
-	rating: 0
+	rating: 1
 	});
 quoteData.push({
+	key: 6,
 	text: 'Whenever you find yourself on the side of the majority, it is time to pause and reflect.',
 	author: 'Mark Twain',
 	views: 0,
-	rating: 0
+	rating: 3
+	});
+quoteData.push({
+	key: 7,
+	text: 'Live as if you were to die tomorrow. Learn as if you were to live forever.',
+	author: 'Mahatma Gandhi',
+	views: 0,
+	rating: 3
 	});
--- main.js
+++ main.js
@@ -1,24 +1,42 @@
+// Constants
+RATING_SCALE = 5;
+
 // Render the quote data on the page
 // layoutStyle is 1(list) or 2(panels)
 var renderData = function(layoutStyle) {
 	// console.log("Rendering the data with layout style: " + layoutStyle);
+	hideInputs();
 	$('.data').empty();
 	console.log(quoteData);
 	for (var i = 0; i <= quoteData.length - 1; i++) {
 
 		var newDataItem = $('<div class="quote-data">');
 		newDataItem.append(
-			'<div>' + quoteData[i].text + '</div>'
+			'<div class="quote-text">' + quoteData[i].text + '</div>'
 		);
 		newDataItem.append(
-			'<div>-' + quoteData[i].author + '</div>'
+			'<div class="quote-author">-' + quoteData[i].author + '</div>'
 		);
+
+		// newDataItem.append(
+		// 	'<div>Rating: ' + quoteData[i].rating + '</div>'
+		// );
+
+		for (var x = 0; x < RATING_SCALE; x++) {
+			// var starRatings = $('<div class="icon-star">')
+			// newDataItem.append(starRatings);
+			if( x < quoteData[i].rating) {
+				newDataItem.append('<div class="icon-star gold-star">');	
+			}
+			else {
+				newDataItem.append('<div class="icon-star">');	
+			}
+		};
+
 		newDataItem.append(
-			'<div>Rating: ' + quoteData[i].rating + '</div>'
+			'<i class="icon-cancel"></i>'
 		);
-		newDataItem.append(
-			'<img id="button-remove" src="http://placehold.it/20&text=Remove">'
-		);
+
 		// Create new DOM element
 		newDataItem.appendTo('.data');
 
@@ -62,8 +80,36 @@ $(document).on('ready', function() {
 		}
 	})
 
-	$(document).on('click', '#button-remove', function() {
+	$(document).on('click', '.icon-cancel', function() {
 		$(this).parent().remove();
 	})
+
+	$('.entry-form').on('submit', function(e) {
+		e.preventDefault();
+		var formData = $('.entry-form').children();
+		quoteData.push({
+			text: formData[0].value,
+			author: formData[1].value,
+			rating: 0
+		})
+		renderData();
+	})
+
+	$('.filter-form').on('submit', function(e) {
+		e.preventDefault();
+		// var filterData = $('.filter-form').children();
+		var filterData = $('.filter-form').find('[name=search]');
+		var authorSearch = {author: filterData[0].value};
+		var textSearch = {text: filterData[0].value};
+		// Research underscore.js filter function
+
+		console.log(filterData[0].value);
+		console.log(authorSearch);
+		console.log(textSearch);
+		console.log(_.contains(quoteData, authorSearch));
+		console.log(_.contains(quoteData, textSearch));
+		console.log(_.contains(quoteData, {text: "*life*"}));
+	})
   
+
 });
